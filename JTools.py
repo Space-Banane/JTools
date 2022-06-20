ebb183a79f3bf3775ad53c85d2d5cc0faa53239a
Paul <64922620+Space-Banane@users.noreply.github.com>
2022-06-20 16:36:39 +0200

    Add files via upload

---
+++ JTools.py
@@ -0,0 +1,25 @@
+import json
+"""
+JTools
+~~~~~~~~~~~~~~~~~~~
+
+FUNCTIONS :
+-Read()
+-Write()
+
+
+https://github.com/Bigboi-Python/JTools
+
+This is a wrapper of the json module(this is easyer to use)
+
+
+JTools 2022 June 
+"""
+
+def Save(data,file):
+    with open(file,"w") as f:
+        json.dump(data,f,ensure_ascii=False,indent=4)
+
+def Read(file):
+    with open(file,"r") as f:
+        data = json.load(f)

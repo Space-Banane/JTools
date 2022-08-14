fcb5f83207069134c7350207927611740780acd8
Space-Banane <paulwaehner923@gmail.com>
2022-08-14 11:59:29 +0200

    Added Exceptions in functions

--- JTools.py
+++ JTools.py
@@ -18,21 +18,26 @@ JTools 2022 June
 import json
 
 def Save(data,file):
-    with open(file,"w") as f:
-        json.dump(data,f,ensure_ascii=False,indent=4)
+    try:
+        with open(file,"w") as f:
+            json.dump(data,f,ensure_ascii=False,indent=4)
+    except:
+        raise Exception("Could not save Data to file")
 
 def Read(file):
-    with open(file,"r") as f:
-        data = json.load(f)
-        print(data)
+    try:
+        with open(file,"r") as f:
+            data = json.load(f)
+            return (data)
+    except:
+        raise Exception("Could not return Data from file")
 
 def load(data,file):
-    with open(file,"r") as f:
-        loaded = json.load(f)
-        data = loaded[data]
-        return data
+    try:
+        with open(file,"r") as f:
+            loaded = json.load(f)
+            data = loaded[data]
+            return data
+    except:
+        raise Exception("Could not load Data from file")
 
-def loadfile(file):
-    with open(file,"r") as f:
-        data = json.load(f)
-        return data
